9b417ab02b4c63f66a76adbdb1ce6a62ba9ec64d
Kenified <kenified@protonmail.com>
2023-01-08 03:18:57 +0100

    Rebranding and falling blocks patch

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,8 @@ pluginManagement {
     }
 }
 
-rootProject.name = "tentacles"
-for (name in listOf("Tentacles-API", "Tentacles-Server")) {
+rootProject.name = "spigan"
+for (name in listOf("Spigan-API", "Spigan-Server")) {
     val projName = name.toLowerCase(Locale.ENGLISH)
     include(projName)
     findProject(":$projName")!!.projectDir = file(name)
